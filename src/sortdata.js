d7a3b06e5f2e426be659ddb26bd973a5910c95c4
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-12 09:53:44 +0300

    hagbdghda

---
+++ src/sortdata.js
@@ -0,0 +1,9 @@
+function sortdata(props) {
+    let sortdata = [false, false, false, true, false, false];
+    let allTrue = booleanArray.every(props => props === true);
+    console.log(allTrue);
+    //props.data.map((user) => 
+    //)
+    return;
+}
+export default sortdata;
